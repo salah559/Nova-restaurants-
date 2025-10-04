7be8f60e862377d86e23d98688e7d9514a512a76
salah559 <142224356+salah559@users.noreply.github.com>
2025-10-04 01:48:47 +0100

    رفع ملفات من ZIP

---
+++ Nova-restaurants-/js/menu_data.js
@@ -0,0 +1,7 @@
+
+const MENU_ITEMS = [
+  {id:1,name_en:'Grilled Chicken', name_ar:'دجاج مشوي', desc_en:'Herb-marinated with roasted vegetables.', desc_ar:'متبل بالأعشاب مع خضار مشوية.', price:'$12', img:'images/dish1.jpg'},
+  {id:2,name_en:'Beef Steak', name_ar:'ستيك لحم', desc_en:'Juicy beef steak with sauce.', desc_ar:'ستيك طري مع صوص الشيف.', price:'$18', img:'images/dish2.jpg'},
+  {id:3,name_en:'Vegetable Pasta', name_ar:'باستا خضار', desc_en:'Healthy veggie pasta.', desc_ar:'باستا بالخضار الصحية.', price:'$10', img:'images/dish3.jpg'},
+  {id:4,name_en:'Chocolate Cake', name_ar:'كيكة الشوكولاتة', desc_en:'Rich chocolate dessert.', desc_ar:'كيك شوكولاتة لذيذ.', price:'$6', img:'images/dish4.jpg'}
+];
